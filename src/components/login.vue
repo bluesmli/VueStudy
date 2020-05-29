e94c585542d7eea5548807deee538afc3f7e2ad9
bluesmli <blues_mli@163.com>
2020-05-29 13:53:28 +0800

    登录优化

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,25 +1,28 @@
 <template>
-	<div class="loginfill">
+	<div class="loginfill" >
 		<el-form :model="loginForm" :rules="rules" ref="loginForm">
-			<div class="formcontainer">
-				<p style=""> 接口测试系统</p>
-				<el-form-item prop="username">
-					<el-input  v-model="loginForm.username" placeholder="用户名"></el-input>
-				</el-form-item>
-				<el-form-item prop="password">
-					<el-input  v-model="loginForm.password" placeholder="请输入密码"  show-password></el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="loginsubmit('loginForm')" class="submit_btn">登陆</el-button>
-				</el-form-item>
-			</div>
+				 <!-- 头像区 -->
+				<div class="formcontainer">
+					<div class="avatar_box">
+						<img src="../assets/flower.jpg" alt="avatar" />
+						<div  class="img-tip"><p>接口测试系统</p></div>
+					</div>
+						<el-form-item prop="username">
+							<el-input style="float:right"  v-model="loginForm.username" placeholder="用户名"></el-input>
+						</el-form-item>
+						<el-form-item prop="password">
+							<el-input  v-model="loginForm.password" placeholder="密码"  show-password></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-button type="primary" @click="loginsubmit('loginForm')" style="width: 100%;" class="submit_btn">登录</el-button>
+						</el-form-item>
+				</div>
 		</el-form>
 	</div>
 </template>
 
 <script>
 	export default {
-		
 		data(){
 			return {
 				loginForm: {
@@ -41,7 +44,8 @@
 		      loginsubmit(formName) {
 		        this.$refs[formName].validate((valid) => {
 		          if (valid) {
-		              this.$message.success("校验成功")
+		              this.$message.success("校验成功");
+					  this.$router.push("/home");
 		          } else {
 		            this.$message.error("校验失败")
 		            return false;
@@ -49,19 +53,14 @@
 		        });
 		      },
 		}
-		
-		
+
 	}
 </script>
 
 
 <style lang="less" scoped>
-
-.fillcontain{
-
-	text-align: center;
-	margin-top: 100px;
-	margin-left: 800px;
+.loginfill{
+	height: 100%;
 }
 
 .formcontainer{
@@ -70,5 +69,22 @@
 	margin-left: 40%;
 }
 
+.avatar_box{
+		position: absolute;
+		width: 100%;
+		top: -100px;
+		left: 0;
+		
+	}
+.img-tip{
+	position: absolute;
+	top: 31%;
+	width: 100%;
+	text-align: center;
+	p{
+		font-size: 34px;
+		font-family: "monospace";
+	}
+}
 
 </style>
